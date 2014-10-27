3971562991776d276a45aad0b50f18fc8968aac3
vlik <vlik1234@gmail.com>
2014-10-27 22:52:05 +0300

    LoaderManger example

--- app/src/main/java/com/example/vlik1234/tmdb/MainActivity.java
+++ app/src/main/java/com/example/vlik1234/tmdb/MainActivity.java
@@ -1,26 +1,108 @@
 package com.example.vlik1234.tmdb;
 
+import android.app.LoaderManager;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.ContentObserver;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements LoaderManager.LoaderCallbacks<String> {
+
+    final String LOG_TAG = "myLogs";
+    static final int LOADER_TIME_ID = 1;
+
+    TextView tvTime;
+    RadioGroup rgTimeFormat;
+    static int lastCheckedId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTime = (TextView) findViewById(R.id.tvTime);
+        rgTimeFormat = (RadioGroup) findViewById(R.id.rgTimeFormat);
 
+        Bundle bndl = new Bundle();
+        bndl.putString(TimeLoader.ARGS_TIME_FORMAT, getTimeFormat());
+        getLoaderManager().initLoader(LOADER_TIME_ID, bndl, this);
+        lastCheckedId = rgTimeFormat.getCheckedRadioButtonId();
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        Loader<String> loader = null;
+        if (id==LOADER_TIME_ID){
+            loader = new TimeLoader(this, args);
+            Log.d(LOG_TAG, "onCreateLoader: "+ loader.hashCode());
+        }
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String result) {
+        Log.d(LOG_TAG,"onFinished fot loader"+loader.hashCode()+", result = "+ result);
+        tvTime.setText(result);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+        Log.d(LOG_TAG, "onLoaderReset for loader "+ loader.hashCode());
+    }
+
+    public void getTimeClick(View v){
+        Loader<String> loader;
+
+        int id = rgTimeFormat.getCheckedRadioButtonId();
+        if (id==lastCheckedId){
+            loader = getLoaderManager().getLoader(LOADER_TIME_ID);
+        } else {
+            Bundle bndl = new Bundle();
+            bndl.putString(TimeLoader.ARGS_TIME_FORMAT, getTimeFormat());
+            loader = getLoaderManager().restartLoader(LOADER_TIME_ID, bndl, this);
+            lastCheckedId = id;
+        }
+        loader.forceLoad();
+    }
+
+    String getTimeFormat(){
+        String result = TimeLoader.TIME_FORMAT_SHORT;
+        switch (rgTimeFormat.getCheckedRadioButtonId()){
+            case R.id.rdShort:
+                result = TimeLoader.TIME_FORMAT_SHORT;break;
+            case R.id.rdLong:
+                result = TimeLoader.TIME_FORMAT_LONG;break;
+        }
+        return result;
+    }
+    public void observerClick(View v){
+        Log.d(LOG_TAG, "obsreverClick");
+        Loader<String> loader = getLoaderManager().getLoader(LOADER_TIME_ID);
+        final ContentObserver observer = loader.new ForceLoadContentObserver();
+        v.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                observer.dispatchChange(false);
+            }
+        }, 5000);
     }
 
     public void onStartSampleClick(View view) {
         Intent intent = new Intent(MainActivity.this, SampleActivity.class);
         intent.putExtra("DocumentInfo", new DocumentInfo("Матроскин", "Длинные", "Белые", "Пушистый"));
         startActivity(intent);
+
     }
 
     @Override
@@ -41,4 +123,6 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
