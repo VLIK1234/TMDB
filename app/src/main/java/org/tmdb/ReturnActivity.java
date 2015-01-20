c4983843ca1f3bb29a485d14faa9cbda79ee5ebb
vlik <vlik1234@gmail.com>
2015-01-20 18:17:18 +0300

    fix back button

---
+++ app/src/main/java/org/tmdb/ReturnActivity.java
@@ -0,0 +1,16 @@
+package org.tmdb;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import org.tmdb.vlik1234.R;
+
+
+public class ReturnActivity extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
